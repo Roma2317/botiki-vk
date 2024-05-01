d8b5113d4a661c8fcbdaea5092618a6a406f8b89
Roma2317 <168623491+Roma2317@users.noreply.github.com>
2024-05-01 13:59:07 +0300

    new-bot

---
+++ www/config.php
@@ -0,0 +1,10 @@
+<?php
+
+define('BOT_BASE_DIRECTORY', '/var/www');
+define('BOT_LOGS_DIRECTORY', BOT_BASE_DIRECTORY.'/logs');
+define('BOT_IMAGES_DIRECTORY', BOT_BASE_DIRECTORY.'/static');
+define('BOT_AUDIO_DIRECTORY', BOT_BASE_DIRECTORY.'/audio');
+
+define('CALLBACK_API_CONFIRMATION_TOKEN', '89c8dfd3'); //Строка для подтверждения адреса сервера из настроек Callback API
+define('VK_API_ACCESS_TOKEN', 'vk1.a.lHdHYy4juc-2551E95IC2nndS5TVFBp2iMGowmQu0URZP7bpz7YYmhEiHZo3VcdD4wKVmXaCsZ0pI4OWjkvkzFYsSpFuPN49MlV7KN8FwiWO9J3F6cJoULeD-UtLs4a1s8c0_o2K4UrXLCWg4UJdldCXNAJ4tLp75_-luYmdHj4kKKRplxXre_j5WeDVZyGhs2nbbrJaxdwpwrXbxPFp-A'); //Ключ доступа сообщества
+define('YANDEX_API_KEY', '30e3213440-61233-1294-b3415-471212369886'); //Ключ для доступа к Yandex Speech Kit
